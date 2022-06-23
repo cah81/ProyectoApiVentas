4c6ac98477a86a3831ce7d25e105098222b92d7c
carlos angulo <carlosenriqueanguloherrera@gmail.com>
2022-06-23 17:23:32 +0200

    Primer Commit

---
+++ springboot-apirest-ventas/src/main/java/com/formacionformacionspring/apirest/SpringbootApirestVentasApplication.java
@@ -0,0 +1,13 @@
+package com.formacionformacionspring.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApirestVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApirestVentasApplication.class, args);
+	}
+
+}
